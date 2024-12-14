0dc3a4dedfbc35696694f95b8f323d05b8b9cfde
Anupriya-2008 <mitudig143@gmail.com>
2024-12-15 01:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Google Apps Script Code
 function doPost(e) {
-  var sheet = SpreadsheetApp.openById("https://docs.google.com/spreadsheets/d/1MPRebdv7K2S2p59axt955nuOfxBimk6f_D7afcpOBeU").getActiveSheet();
+  var sheet = SpreadsheetApp.openById("https://docs.google.com/spreadsheets/d/1MPRebdv7K2S2p59axt955nuOfxBimk6f_D7afcpOBeU/edit?usp=drivesdk").getActiveSheet();
   var data = JSON.parse(e.postData.contents);
 
   // Append form data to the spreadsheet
